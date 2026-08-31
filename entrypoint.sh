9dc875dc9199d209345b1d95dafc163d7af55c42
Chamara Pushpakumara <d.c.p.kumara.1995@gmail.com>
2026-08-31 22:56:16 +0530

    Create admin user on deployment

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,10 @@ echo "Running database migrations..."
 
 python manage.py migrate --noinput
 
+echo "Creating Admin user..."
+
+python manage.py shell -c "from django.contrib.auth.models import User; u, created = User.objects.get_or_create(username='Admin'); u.set_password('Admin@#123'); u.is_superuser=True; u.is_staff=True; u.is_active=True; u.save(); print('Admin user ready')"
+
 echo "Starting Gunicorn..."
 
 exec gunicorn --bind 0.0.0.0:8000 --workers 2 config.wsgi:application
